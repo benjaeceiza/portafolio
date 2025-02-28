6e2216694102b54fe8b719b7265f9cd221e09752
Benjamin Eceiza <benjaceiza400@gmail.com>
2025-02-28 01:31:08 -0300

    .

--- js/main.js
+++ js/main.js
@@ -3,28 +3,10 @@
 window.addEventListener('scroll', function () {
   
     let animacionTextos = document.getElementById("sobre-mi")
-    let contenedorContactoFlotante = document.getElementById("contenedor-contacto-flotante")
     let posicionObj1 = animacionTextos.getBoundingClientRect().top;
-    let tamanoPantalla = window.innerHeight / 1;
     let tamanoPantallaTextos = window.innerHeight;
 
 
-
-
-    const agregarEstilo = (animacion, textosProyectos) => {
-        animacion.style.height = "500px"
-        textosProyectos.style.animation = "opacidad 1s "
-
-    }
-    
-    const quitarEstilo = (animacion, textosProyectos) => {
-        animacion.style.height = "220px"
-        textosProyectos.style.animation = "none";
-    }
-
-   
-
- 
 
     if (posicionObj1 = tamanoPantallaTextos) {
         animacionTextos.style.animation = "opacidad 2s ease-out"
@@ -37,6 +19,7 @@ let proyectoUno = document.getElementById("proyecto-1")
 let proyectoDos = document.getElementById("proyecto-2")
 let proyectoTres = document.getElementById("proyecto-3")
 let proyectoCuatro = document.getElementById("proyecto-4")
+let proyectoCinco = document.getElementById("proyecto-5")
 let botonCerrarContacto = document.getElementById("cerrar-contacto")
 let botonContacto = document.getElementById("boton-contacto")
 let contactoFlotante = document.getElementById("contacto-flotante")
@@ -51,7 +34,10 @@ proyectoUno.onmouseleave = () => revertSize(proyectoUno)
 proyectoDos.onmouseleave = () => revertSize(proyectoDos)
 proyectoTres.onmouseleave = () => revertSize(proyectoTres)
 proyectoCuatro.onmouseleave = () => revertSize(proyectoCuatro)
+proyectoCinco.onmouseleave = () => revertSize(proyectoCinco)
 
  
 botonContacto.onclick = () => contactoFlotante.style.display = "flex"
 botonCerrarContacto.onclick = () => contactoFlotante.style.display = "none"
+botonCerrarContacto.onmouseenter = () => botonCerrarContacto.src="./assets/iconos/cerrar-hover.png"
+botonCerrarContacto.onmouseleave = () => botonCerrarContacto.src="./assets/iconos/cerrar.png"
