5d6020b4a852fac78fbca57c73bb0532d877d3df
Benjamin Eceiza <benjaceiza400@gmail.com>
2025-03-06 17:47:01 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 window.addEventListener('scroll', function () {
-  
+
     let animacionTextos = document.getElementById("sobre-mi")
     let posicionObj1 = animacionTextos.getBoundingClientRect().top;
     let tamanoPantallaTextos = window.innerHeight;
@@ -23,21 +23,55 @@ let proyectoCinco = document.getElementById("proyecto-5")
 let botonCerrarContacto = document.getElementById("cerrar-contacto")
 let botonContacto = document.getElementById("boton-contacto")
 let contactoFlotante = document.getElementById("contacto-flotante")
+let contenedorContactoFlotante = document.getElementById("contenedor-contacto-flotante")
+let iconoMail = document.getElementById("icono-mail")
+let mainFondo = document.getElementById("body")
+
+
+
 
 const revertSize = (animacion) => {
     animacion.style.scale = "1"
     animacion.style.transition = "all .5s"
 }
 
+const animationButtonContact = () => {
+    iconoMail.src = "./assets/iconos/mail-hover.png";
+    botonContacto.style.backgroundColor = "white"
+    botonContacto.style.transition = "all .5s"
+}
+const deleteAnimationButtonContact = () => {
+    iconoMail.src = "./assets/iconos/mail.png";
+    botonContacto.style.backgroundColor = "#5d2f72";
+    botonContacto.style.transition = "all .5s"
+}
 
+const mostrarContactoFlotante = () => {
+
+    contactoFlotante.style.display = "flex"
+    mainFondo.style.overflow = "hidden"
+}
+const cerrarContactoFlotante = () => {
+    
+    contactoFlotante.style.display = "none"
+    mainFondo.style.overflow = "auto"  
+    
+    
+}
+
+
+// ANIMACION PROYECTOS
 proyectoUno.onmouseleave = () => revertSize(proyectoUno)
 proyectoDos.onmouseleave = () => revertSize(proyectoDos)
 proyectoTres.onmouseleave = () => revertSize(proyectoTres)
 proyectoCuatro.onmouseleave = () => revertSize(proyectoCuatro)
 proyectoCinco.onmouseleave = () => revertSize(proyectoCinco)
 
- 
-botonContacto.onclick = () => contactoFlotante.style.display = "flex"
-botonCerrarContacto.onclick = () => contactoFlotante.style.display = "none"
-botonCerrarContacto.onmouseenter = () => botonCerrarContacto.src="./assets/iconos/cerrar-hover.png"
-botonCerrarContacto.onmouseleave = () => botonCerrarContacto.src="./assets/iconos/cerrar.png"
+// EVENTOS CONTACTO FLOTANTE
+
+botonContacto.onmouseenter = () => animationButtonContact();
+botonContacto.onmouseleave = () => deleteAnimationButtonContact();
+botonContacto.onclick = () => mostrarContactoFlotante();
+botonCerrarContacto.onclick = () => cerrarContactoFlotante();
+botonCerrarContacto.onmouseenter = () => botonCerrarContacto.src = "./assets/iconos/cerrar-hover.png"
+botonCerrarContacto.onmouseleave = () => botonCerrarContacto.src = "./assets/iconos/cerrar.png"
